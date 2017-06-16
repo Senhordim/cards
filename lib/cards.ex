c981f6014b5f9ad33159bc45f011cd7842aff4f9
Diego Collares <diego.collares@gmail.com>
2017-06-16 14:21:22 -0300

    create method hello, ceate_deck and shuffle

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,15 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello
-      :world
-
-  """
   def hello do
-    :world
+    "Hi there!"
   end
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
 end
